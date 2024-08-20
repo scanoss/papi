0cfd9ed54df4db6b5727270e17b6e0b6ed7b2f8c
scanoss-qg <quique.goni@scanoss.com>
2024-08-20 19:52:10 -0300

    Moved crypto request AlgorithmsInMajor to AlgorithmsInRange

--- api/cryptographyv2/scanoss-cryptography.pb.gw.go
+++ api/cryptographyv2/scanoss-cryptography.pb.gw.go
@@ -100,7 +100,7 @@ func local_request_Cryptography_GetAlgorithms_0(ctx context.Context, marshaler r
 
 }
 
-func request_Cryptography_GetAlgorithmsInMajor_0(ctx context.Context, marshaler runtime.Marshaler, client CryptographyClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+func request_Cryptography_GetAlgorithmsInRange_0(ctx context.Context, marshaler runtime.Marshaler, client CryptographyClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq commonv2.PurlRequest
 	var metadata runtime.ServerMetadata
 
@@ -112,12 +112,12 @@ func request_Cryptography_GetAlgorithmsInMajor_0(ctx context.Context, marshaler 
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := client.GetAlgorithmsInMajor(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.GetAlgorithmsInRange(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
 
-func local_request_Cryptography_GetAlgorithmsInMajor_0(ctx context.Context, marshaler runtime.Marshaler, server CryptographyServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+func local_request_Cryptography_GetAlgorithmsInRange_0(ctx context.Context, marshaler runtime.Marshaler, server CryptographyServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq commonv2.PurlRequest
 	var metadata runtime.ServerMetadata
 
@@ -129,7 +129,7 @@ func local_request_Cryptography_GetAlgorithmsInMajor_0(ctx context.Context, mars
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := server.GetAlgorithmsInMajor(ctx, &protoReq)
+	msg, err := server.GetAlgorithmsInRange(ctx, &protoReq)
 	return msg, metadata, err
 
 }
@@ -190,7 +190,7 @@ func RegisterCryptographyHandlerServer(ctx context.Context, mux *runtime.ServeMu
 
 	})
 
-	mux.Handle("POST", pattern_Cryptography_GetAlgorithmsInMajor_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("POST", pattern_Cryptography_GetAlgorithmsInRange_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
@@ -198,12 +198,12 @@ func RegisterCryptographyHandlerServer(ctx context.Context, mux *runtime.ServeMu
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
 		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/scanoss.api.cryptography.v2.Cryptography/GetAlgorithmsInMajor", runtime.WithHTTPPathPattern("/api/v2/cryptography/algorithmsInMajor"))
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/scanoss.api.cryptography.v2.Cryptography/GetAlgorithmsInRange", runtime.WithHTTPPathPattern("/api/v2/cryptography/algorithmsInRange"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_Cryptography_GetAlgorithmsInMajor_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_Cryptography_GetAlgorithmsInRange_0(annotatedContext, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
 		if err != nil {
@@ -211,7 +211,7 @@ func RegisterCryptographyHandlerServer(ctx context.Context, mux *runtime.ServeMu
 			return
 		}
 
-		forward_Cryptography_GetAlgorithmsInMajor_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Cryptography_GetAlgorithmsInRange_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -300,25 +300,25 @@ func RegisterCryptographyHandlerClient(ctx context.Context, mux *runtime.ServeMu
 
 	})
 
-	mux.Handle("POST", pattern_Cryptography_GetAlgorithmsInMajor_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("POST", pattern_Cryptography_GetAlgorithmsInRange_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
 		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/scanoss.api.cryptography.v2.Cryptography/GetAlgorithmsInMajor", runtime.WithHTTPPathPattern("/api/v2/cryptography/algorithmsInMajor"))
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/scanoss.api.cryptography.v2.Cryptography/GetAlgorithmsInRange", runtime.WithHTTPPathPattern("/api/v2/cryptography/algorithmsInRange"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_Cryptography_GetAlgorithmsInMajor_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_Cryptography_GetAlgorithmsInRange_0(annotatedContext, inboundMarshaler, client, req, pathParams)
 		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
 		if err != nil {
 			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_Cryptography_GetAlgorithmsInMajor_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Cryptography_GetAlgorithmsInRange_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -330,7 +330,7 @@ var (
 
 	pattern_Cryptography_GetAlgorithms_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v2", "cryptography", "algorithms"}, ""))
 
-	pattern_Cryptography_GetAlgorithmsInMajor_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v2", "cryptography", "algorithmsInMajor"}, ""))
+	pattern_Cryptography_GetAlgorithmsInRange_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v2", "cryptography", "algorithmsInRange"}, ""))
 )
 
 var (
@@ -338,5 +338,5 @@ var (
 
 	forward_Cryptography_GetAlgorithms_0 = runtime.ForwardResponseMessage
 
-	forward_Cryptography_GetAlgorithmsInMajor_0 = runtime.ForwardResponseMessage
+	forward_Cryptography_GetAlgorithmsInRange_0 = runtime.ForwardResponseMessage
 )
